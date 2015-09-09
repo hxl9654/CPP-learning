e28229f191993b8e46478b97e42d371ba2dbb3ca
Xianglong He <admin@hxlxz.com>
2015-09-09 14:21:43 +0800

    3-5

---
+++ 第三章/3-5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+	using namespace std;
+	char ch;
+
+	cout << "Enter a char: " << endl;
+	cin >> ch;
+	cout << "Hola! ";
+	cout << "Thank you for the " << ch << " character." << endl;
+	return 0;
+}
