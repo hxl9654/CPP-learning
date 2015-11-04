afff0800835ee3d3d3e090370cfd98664edf6bb6
Xianglong He <admin@hxlxz.com>
2015-11-04 20:56:44 +0800

    将链表节点改为动态内存分配，修复节点被异常释放的错误

--- 器件管理系统/main.cpp
+++ 器件管理系统/main.cpp
@@ -14,19 +14,19 @@ using namespace std;
  *版  本：1.0
  *日  期：2015-11-04
 *///////////////////////////////////////////////////////////////////////////////////
-class NotFinishException: public exception
+class NotFinishException : public exception
 {
 public:
-    NotFinishException() throw() { }
-    virtual ~NotFinishException() throw(){ }
-    NotFinishException(string msg) throw(){message = msg;}
-    const char *what() const throw();
+	NotFinishException() throw() { }
+	virtual ~NotFinishException() throw() { }
+	NotFinishException(string msg) throw() { message = msg; }
+	const char *what() const throw();
 private:
-    string message;
+	string message;
 };
 const char * NotFinishException::what() const throw()
 {
-    return "该功能未完成";
+	return "该功能未完成";
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *类  名：NoValueException
@@ -38,19 +38,19 @@ const char * NotFinishException::what() const throw()
  *版  本：1.0
  *日  期：2015-11-04
 *///////////////////////////////////////////////////////////////////////////////////
-class NoValueException: public exception
+class NoValueException : public exception
 {
 public:
-    NoValueException() throw() { }
-    virtual ~NoValueException() throw(){ }
-    NoValueException(string msg) throw(){message = msg;}
-    const char *what() const throw();
+	NoValueException() throw() { }
+	virtual ~NoValueException() throw() { }
+	NoValueException(string msg) throw() { message = msg; }
+	const char *what() const throw();
 private:
-    string message;
+	string message;
 };
 const char * NoValueException::what() const throw()
 {
-    return "该对象没有相应属性";
+	return "该对象没有相应属性";
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *类  名：Base
@@ -62,24 +62,24 @@ const char * NoValueException::what() const throw()
 class Base
 {
 public:
-    string name;    //该器件的名字
-    int num;        //库存数量
-    double value;   //器件单价
-    virtual void increase(int n) const;   //器件入库（n：数量）
-    virtual void decrease(int n) const;   //器件出库（n：数量）
-    virtual double getValueSum() const;   //获取该器件的总价值（返回：总价值）
+	string name;    //该器件的名字
+	int num;        //库存数量
+	double value;   //器件单价
+	virtual void increase(int n) const;   //器件入库（n：数量）
+	virtual void decrease(int n) const;   //器件出库（n：数量）
+	virtual double getValueSum() const;   //获取该器件的总价值（返回：总价值）
 };
 void Base::increase(int n) const
 {
-    throw NoValueException("num");
+	throw NoValueException("num");
 }
 void Base::decrease(int n) const
 {
-    throw NoValueException("num");
+	throw NoValueException("num");
 }
 double Base::getValueSum() const
 {
-    throw NoValueException("value");
+	throw NoValueException("value");
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *类  名：Thing_withoutNum
@@ -88,14 +88,14 @@ double Base::getValueSum() const
  *版  本：1.0
  *日  期：2015-11-04
 *///////////////////////////////////////////////////////////////////////////////////
-class Thing_withoutNum: public Base
+class Thing_withoutNum : public Base
 {
 public:
-    double getValueSum();   //获取该器件的总价值（返回：总价值）
+	double getValueSum();   //获取该器件的总价值（返回：总价值）
 };
 double Thing_withoutNum::getValueSum()
 {
-    return value;
+	return value;
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *类  名：Thing_withoutValue
@@ -104,19 +104,19 @@ double Thing_withoutNum::getValueSum()
  *版  本：1.0
  *日  期：2015-11-04
 *///////////////////////////////////////////////////////////////////////////////////
-class Thing_withoutValue: public Base
+class Thing_withoutValue : public Base
 {
 public:
-    void increase(int n);   //器件入库（n：数量）
-    void decrease(int n);   //器件出库（n：数量）
+	void increase(int n);   //器件入库（n：数量）
+	void decrease(int n);   //器件出库（n：数量）
 };
 void Thing_withoutValue::increase(int n)
 {
-    num += n;
+	num += n;
 }
 void Thing_withoutValue::decrease(int n)
 {
-    num -= n;
+	num -= n;
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *类  名：Thing
@@ -128,24 +128,24 @@ void Thing_withoutValue::decrease(int n)
  *版  本：1.0
  *日  期：2015-11-04
 *///////////////////////////////////////////////////////////////////////////////////
-class Thing: public Base
+class Thing : public Base
 {
 public:
-    void increase(int n);   //器件入库（n：数量）
-    void decrease(int n);   //器件出库（n：数量）
-    double getValueSum();   //获取该器件的总价值（返回：总价值）
+	void increase(int n);   //器件入库（n：数量）
+	void decrease(int n);   //器件出库（n：数量）
+	double getValueSum();   //获取该器件的总价值（返回：总价值）
 };
 void Thing::increase(int n)
 {
-    num += n;
+	num += n;
 }
 void Thing::decrease(int n)
 {
-    num -= n;
+	num -= n;
 }
 double Thing::getValueSum()
 {
-    return value * num;
+	return value * num;
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *类型名：Things
@@ -154,11 +154,11 @@ double Thing::getValueSum()
  *版  本：1.0
  *日  期：2015-11-04
 *///////////////////////////////////////////////////////////////////////////////////
-struct Things{
-        Base* thing;
-        int type;
-        Things* next = NULL;
-    }head;
+struct Things {
+	Base* thing;
+	int type;
+	Things* next = NULL;
+}head;
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：menu
  *功  能：显示程序菜单，并返回用户的选择。
@@ -174,16 +174,16 @@ struct Things{
 *///////////////////////////////////////////////////////////////////////////////////
 int menu()
 {
-    system("cls");
-    cout  << endl  << endl << "\t\t\t\t欢迎使用器件管理系统" << endl  << endl;
-    cout << "----------------------------------------------------------------------------" << endl << endl;
-    cout << "\t1.录入器件信息" << endl << "\t2.删除已有器件" << endl << "\t3.查询器件信息" << endl;
-    cout << "\t4.修改器件信息" << endl << "\t5.器件出库与入库" << endl<< "\t6.从文件读入数据" << endl;
-    cout << "\t7.保存数据到文件" << endl << "\t0.退出系统" << endl << endl << endl;
-    cout << "请选择您要进行的操作：";
-    int n;
-    cin >> n;
-    return n;
+	system("cls");
+	cout << endl << endl << "\t\t\t\t欢迎使用器件管理系统" << endl << endl;
+	cout << "----------------------------------------------------------------------------" << endl << endl;
+	cout << "\t1.录入器件信息" << endl << "\t2.删除已有器件" << endl << "\t3.查询器件信息" << endl;
+	cout << "\t4.修改器件信息" << endl << "\t5.器件出库与入库" << endl << "\t6.从文件读入数据" << endl;
+	cout << "\t7.保存数据到文件" << endl << "\t0.退出系统" << endl << endl << endl;
+	cout << "请选择您要进行的操作：";
+	int n;
+	cin >> n;
+	return n;
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：Exit_confirm
@@ -196,15 +196,15 @@ int menu()
 *///////////////////////////////////////////////////////////////////////////////////
 bool Exit_confirm()
 {
-    system("cls");
-    cout << "您确定要退出本系统吗？所有未保存的数据将丢失！" << endl;
-    cout << "确定请输入Y，取消请输入N：";
-    char t = ' ';
-    while(t == ' ' || t == '\n' || t == '\r')
-        cin >> t;
-    if(t == 'y' || t == 'Y')
-        return true;
-    else return false;
+	system("cls");
+	cout << "您确定要退出本系统吗？所有未保存的数据将丢失！" << endl;
+	cout << "确定请输入Y，取消请输入N：";
+	char t = ' ';
+	while (t == ' ' || t == '\n' || t == '\r')
+		cin >> t;
+	if (t == 'y' || t == 'Y')
+		return true;
+	else return false;
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：CheckName
@@ -217,17 +217,17 @@ bool Exit_confirm()
 *///////////////////////////////////////////////////////////////////////////////////
 bool CheckName(string name)
 {
-    if(name == "" || name.length() == 0)
-        return false;
-    Things* p = head.next;
-    while(p)
-    {
-        if(p->thing->name == name)break;
-        p = p -> next;
-    }
-    if(p)
-        return false;
-    else return true;
+	if (name == "" || name.length() == 0)
+		return false;
+	Things* p = head.next;
+	while (p)
+	{
+		if (p->thing->name == name)break;
+		p = p->next;
+	}
+	if (p)
+		return false;
+	else return true;
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：Devices_new
@@ -240,132 +240,132 @@ bool CheckName(string name)
 *///////////////////////////////////////////////////////////////////////////////////
 void Devices_new()
 {
-    system("cls");
-    cout << endl << "请选择器件类型（1.普通器件；2.不计算价值的器件；3.独一无二的器件；4.独一无二且无价值的器件；0.返回）：";
-    int t;
-    cin >> t;
-    switch(t)
-    {
-        case(0):return ;
-        case(1):
-            {
-                string name;
-                int num;
-                double value;
-                lable_Devices_new11:
-                cout << "请输入器件的名称：";
-                cin >> name;
-                if(!CheckName(name))goto lable_Devices_new11;
-                lable_Devices_new12:
-                cout << "请输入器件的数量：";
-                cin >> num;
-                if(num <= 0)goto lable_Devices_new12;
-                lable_Devices_new13:
-                cout << "请输入器件的价值：";
-                cin >> value;
-                if(value <= 0)goto lable_Devices_new13;
+	system("cls");
+	cout << endl << "请选择器件类型（1.普通器件；2.不计算价值的器件；3.独一无二的器件；4.独一无二且无价值的器件；0.返回）：";
+	int t;
+	cin >> t;
+	switch (t)
+	{
+	case(0) : return;
+	case(1) :
+	{
+		string name;
+		int num;
+		double value;
+	lable_Devices_new11:
+		cout << "请输入器件的名称：";
+		cin >> name;
+		if (!CheckName(name))goto lable_Devices_new11;
+	lable_Devices_new12:
+		cout << "请输入器件的数量：";
+		cin >> num;
+		if (num <= 0)goto lable_Devices_new12;
+	lable_Devices_new13:
+		cout << "请输入器件的价值：";
+		cin >> value;
+		if (value <= 0)goto lable_Devices_new13;
 
-                Things* p = &head;
-                Things add;
-                while(p -> next)
-                    p = p -> next;
-                p -> next = &add;
+		Things* p = &head;
+		Things* add = (Things*)malloc(sizeof(Things));
+		while (p->next)
+			p = p->next;
+		p->next = add;
 
-                Thing* a = new Thing();
-                a -> name = name;
-                a -> num = num;
-                a -> value = value;
+		Thing* a = new Thing();
+		a->name = name;
+		a->num = num;
+		a->value = value;
 
-                add.type = 1;
-                add.thing = a;
-                add.next = NULL;
-                return;
-            }
-        case(2):
-            {
-                string name;
-                int num;
-                lable_Devices_new21:
-                cout << "请输入器件的名称：";
-                cin >> name;
-                if(!CheckName(name))goto lable_Devices_new21;
-                lable_Devices_new22:
-                cout << "请输入器件的数量：";
-                cin >> num;
-                if(num <= 0)goto lable_Devices_new22;
+		add->type = 1;
+		add->thing = a;
+		add->next = NULL;
+		return;
+	}
+	case(2) :
+	{
+		string name;
+		int num;
+	lable_Devices_new21:
+		cout << "请输入器件的名称：";
+		cin >> name;
+		if (!CheckName(name))goto lable_Devices_new21;
+	lable_Devices_new22:
+		cout << "请输入器件的数量：";
+		cin >> num;
+		if (num <= 0)goto lable_Devices_new22;
 
-                Things* p = &head;
-                Things add;
-                while(p -> next)
-                    p = p -> next;
-                p -> next = &add;
+		Things* p = &head;
+		Things* add = (Things*)malloc(sizeof(Things));
+		while (p->next)
+			p = p->next;
+		p->next = add;
 
-                Thing_withoutValue* a = new Thing_withoutValue();
-                a -> name = name;
-                a -> num = num;
+		Thing_withoutValue* a = new Thing_withoutValue();
+		a->name = name;
+		a->num = num;
 
-                add.type = 2;
-                add.thing = a;
-                add.next = NULL;
-                return;
-            }
-        case(3):
-            {
-                string name;
-                double value;
-                lable_Devices_new31:
-                cout << "请输入器件的名称：";
-                cin >> name;
-                if(!CheckName(name))goto lable_Devices_new31;
-                lable_Devices_new32:
-                cout << "请输入器件的价值：";
-                cin >> value;
-                if(value <= 0)goto lable_Devices_new32;
+		add->type = 2;
+		add->thing = a;
+		add->next = NULL;
+		return;
+	}
+	case(3) :
+	{
+		string name;
+		double value;
+	lable_Devices_new31:
+		cout << "请输入器件的名称：";
+		cin >> name;
+		if (!CheckName(name))goto lable_Devices_new31;
+	lable_Devices_new32:
+		cout << "请输入器件的价值：";
+		cin >> value;
+		if (value <= 0)goto lable_Devices_new32;
 
-                Things* p = &head;
-                Things add;
-                while(p -> next)
-                    p = p -> next;
-                p -> next = &add;
+		Things* p = &head;
+		Things* add = (Things*)malloc(sizeof(Things));
+		while (p->next)
+			p = p->next;
+		p->next = add;
 
-                Thing_withoutNum* a = new Thing_withoutNum();
-                a -> name = name;
-                a -> value = value;
+		Thing_withoutNum* a = new Thing_withoutNum();
+		a->name = name;
+		a->value = value;
 
-                add.type = 3;
-                add.thing = a;
-                add.next = NULL;
-                return;
-            }
-        case(4):
-            {
-                string name;
-                lable_Devices_new41:
-                cout << "请输入器件的名称：";
-                cin >> name;
-                if(!CheckName(name))goto lable_Devices_new41;
+		add->type = 3;
+		add->thing = a;
+		add->next = NULL;
+		return;
+	}
+	case(4) :
+	{
+		string name;
+	lable_Devices_new41:
+		cout << "请输入器件的名称：";
+		cin >> name;
+		if (!CheckName(name))goto lable_Devices_new41;
 
-                Things* p = &head;
-                Things add;
-                while(p -> next)
-                    p = p -> next;
-                p -> next = &add;
+		Things* p = &head;
+		Things* add = (Things*)malloc(sizeof(Things));
+		while (p->next)
+			p = p->next;
+		p->next = add;
 
-                Base* a = new Base();
-                a -> name = name;
+		Base* a = new Base();
+		a->name = name;
 
-                add.type = 4;
-                add.thing = a;
-                add.next = NULL;
-                return;
-            }
-        default:
-            {
-                 cout << "选择有误，请重新选择" << endl;
-                 Devices_new();
-                 return;
-            }
-    }
+		add->type = 4;
+		add->thing = a;
+		add->next = NULL;
+		return;
+	}
+	default:
+	{
+		cout << "选择有误，请重新选择" << endl;
+		Devices_new();
+		return;
+	}
+	}
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：Devices_delete
@@ -378,41 +378,41 @@ void Devices_new()
 *///////////////////////////////////////////////////////////////////////////////////
 void Devices_delete()
 {
-    system("cls");
-    string name;
-    cout << "请输入要删除的器件名称：";
-    cin >> name;
+	system("cls");
+	string name;
+	cout << "请输入要删除的器件名称：";
+	cin >> name;
 
-    Things* p = head.next;
-    Things* q = &head;
-    while(p)
-    {
-        if(p->thing->name == name)break;
-        q = p;
-        p = p -> next;
-    }
-    if(p)
-    {
-        cout << "请确认你要删除的器件的信息：" << endl;
-        cout << "名称：" << p->thing->name << endl;
-        if(p->type == 1 || p->type == 2)
-            cout << "数量：" << p->thing->num << endl;
-        if(p->type == 1 || p->type == 3)
-            cout << "价值：" << p->thing->value << endl;
-        cout << "你确定要删除该器件吗？(Y/N):";
-        char t;
-        cin >> t;
-        if(t == 'y' || t == 'Y')
-        {
-            q -> next = p -> next;
-            free(p);
-        }
-    }
-    else
-    {
-        cout << "未找到该器件";
-        system("pause");
-    }
+	Things* p = head.next;
+	Things* q = &head;
+	while (p)
+	{
+		if (p->thing->name == name)break;
+		q = p;
+		p = p->next;
+	}
+	if (p)
+	{
+		cout << "请确认你要删除的器件的信息：" << endl;
+		cout << "名称：" << p->thing->name << endl;
+		if (p->type == 1 || p->type == 2)
+			cout << "数量：" << p->thing->num << endl;
+		if (p->type == 1 || p->type == 3)
+			cout << "价值：" << p->thing->value << endl;
+		cout << "你确定要删除该器件吗？(Y/N):";
+		char t;
+		cin >> t;
+		if (t == 'y' || t == 'Y')
+		{
+			q->next = p->next;
+			free(p);
+		}
+	}
+	else
+	{
+		cout << "未找到该器件";
+		system("pause");
+	}
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：Devices_search
@@ -425,7 +425,7 @@ void Devices_delete()
 *///////////////////////////////////////////////////////////////////////////////////
 void Devices_search()
 {
-    throw NotFinishException();
+	throw NotFinishException();
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：Devices_change
@@ -438,7 +438,7 @@ void Devices_search()
 *///////////////////////////////////////////////////////////////////////////////////
 void Devices_change()
 {
-    throw NotFinishException();
+	throw NotFinishException();
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：Devices_new
@@ -451,7 +451,7 @@ void Devices_change()
 *///////////////////////////////////////////////////////////////////////////////////
 void Devices_io()
 {
-    throw NotFinishException();
+	throw NotFinishException();
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：File_read
@@ -464,7 +464,7 @@ void Devices_io()
 *///////////////////////////////////////////////////////////////////////////////////
 void File_read()
 {
-    throw NotFinishException();
+	throw NotFinishException();
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：File_write
@@ -477,7 +477,7 @@ void File_read()
 *///////////////////////////////////////////////////////////////////////////////////
 void File_write()
 {
-    throw NotFinishException();
+	throw NotFinishException();
 }
 /*//////////////////////////////////////////////////////////////////////////////////
  *函数名：main
@@ -490,33 +490,33 @@ void File_write()
 *///////////////////////////////////////////////////////////////////////////////////
 int main()
 {
-    while(1)
-    {
-        int c = menu();
-        switch(c)
-        {
-            case(0):
-                {
-                    if(Exit_confirm())
-                        return 0;
-                    else menu();
-                    break;
-                }
-            case(1):Devices_new();break;
-            case(2):Devices_delete();break;
-            case(3):Devices_search();break;
-            case(4):Devices_change();break;
-            case(5):Devices_io();break;
-            case(6):File_read();break;
-            case(7):File_write();break;
-            default:
-                {
-                    cout << "输入有误，请重新输入。" << endl;
-                    system("pause");
-                    menu();
-                    break;
-                }
-        }
-    }
-    return 0;
+	while (1)
+	{
+		int c = menu();
+		switch (c)
+		{
+		case(0) :
+		{
+			if (Exit_confirm())
+				return 0;
+			else menu();
+			break;
+		}
+		case(1) : Devices_new(); break;
+		case(2) : Devices_delete(); break;
+		case(3) : Devices_search(); break;
+		case(4) : Devices_change(); break;
+		case(5) : Devices_io(); break;
+		case(6) : File_read(); break;
+		case(7) : File_write(); break;
+		default:
+		{
+			cout << "输入有误，请重新输入。" << endl;
+			system("pause");
+			menu();
+			break;
+		}
+		}
+	}
+	return 0;
 }
